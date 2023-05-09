eb5ab2c73f7f6d219026cb62eca1c7ef9e542d7d
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-05-09 13:10:59 +0600

    *Project about functionality of keyboard dismissing also using swift packages.

--- HidingKeyboardProject/ViewController.swift
+++ HidingKeyboardProject/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var professionTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.delegate = self
+        ageTextField.delegate = self
+        professionTextField.delegate = self
+        addressTextField.delegate = self
     }
+ 
+    //keyboard take return kore dibe done button er maddhome.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameTextField.resignFirstResponder()
+        ageTextField.resignFirstResponder()
+        professionTextField.resignFirstResponder()
+        addressTextField.resignFirstResponder()
 
-
+        return true
+    }
+    
+    // View er je kono jaigai click korlei keyboard dismiss hoye jabe.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
 }
 
